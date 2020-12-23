8b23dc9e34d9a45581384e3053aa7992bdb92071
thesugar <thesugar87@gmail.com>
2020-12-24 02:29:40 +0900

    process differently depending on the type of http method

--- node-js-http2/dist/index.js
+++ node-js-http2/dist/index.js
@@ -5,11 +5,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const server = http_1.default.createServer((req, res) => {
-    console.info(`[${new Date()}] Requested by ${req.connection.remoteAddress}`);
+    const now = new Date();
+    console.info(`[${now}] Requested by ${req.connection.remoteAddress}`);
     res.writeHead(200, {
         'Content-Type': 'text/plain; charset=utf-8'
     });
-    res.write(req.headers['user-agent']);
+    switch (req.method) {
+        case 'GET':
+            res.write(`GET ${req.url}`);
+            break;
+        case 'POST':
+            res.write(`POST ${req.url}`);
+            let rawData = '';
+            // req（リクエストオブジェクト）はイベントを発行するオブジェクト
+            // データを受け取った際には 'data' というイベントが発生する。
+            req.on('data', (chunk) => {
+                rawData += chunk;
+            }).on('end', () => {
+                console.info(`[${now}] Data Posted: ${rawData}`);
+            });
+            break;
+        case 'DELETE':
+            res.write(`DELETE ${req.url}`);
+        default:
+            break;
+    }
     res.end();
 }).on('error', e => {
     console.error(`[${new Date()}] Server Error`, e);
--- node-js-http2/src/index.ts
+++ node-js-http2/src/index.ts
@@ -3,11 +3,38 @@
 import http from 'http'
 
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-    console.info(`[${new Date()}] Requested by ${req.connection.remoteAddress}`)
+    const now = new Date()
+    console.info(`[${now}] Requested by ${req.connection.remoteAddress}`)
     res.writeHead(200, {
         'Content-Type': 'text/plain; charset=utf-8'
     })
-    res.write(req.headers['user-agent'])
+    
+    switch (req.method) {
+        case 'GET':
+            res.write(`GET ${req.url}`)
+            break
+    
+        case 'POST':
+            res.write(`POST ${req.url}`)
+            let rawData = ''
+
+            // req（リクエストオブジェクト）はイベントを発行するオブジェクト
+            // データを受け取った際には 'data' というイベントが発生する。
+            req.on('data', (chunk) => {
+                rawData += chunk
+            }).on('end', () => {
+                console.info(`[${now}] Data Posted: ${rawData}`)
+            })
+            break
+
+        case 'DELETE':
+            res.write(`DELETE ${req.url}`)
+            break
+
+        default:
+            break
+    }
+
     res.end()
 }).on('error', e => {
     console.error(`[${new Date()}] Server Error`, e)
